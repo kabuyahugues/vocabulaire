5e6866fd9b67d8750b6c2e7addc051b5855042b6
kabuyahugues <hugues_beya@hotmail.fr>
2025-06-11 10:04:09 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+console.log('helloWorld');
+
+///// variable  //////
+
+let btnSend = document.querySelector('.send');
+let btnDelete = document.querySelector('.erase');
+
+let inputOriginal = document.querySelector('.from')
+
+///// class  //////
+
+class Data {
+  constructor(data = []) {
+    this.data = data
+  }
+
+  setdata(){
+
+  }
+}
+
+let data = new DataTransfer();
+
+///// function  //////
+
+
+
+///// event  //////
+///// function apply  //////
